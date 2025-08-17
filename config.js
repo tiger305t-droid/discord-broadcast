9ff52b5fbde7b9429a7bb6d0df3eed1bac7b81d9
tiger305t-droid <tiger305t@gmail.com>
2025-08-17 23:09:45 +0300

    Update config.js

--- config.js
+++ config.js
@@ -11,15 +11,15 @@ module.exports = {
         defaultLanguage: 'ar', // ar | en لغة البوت
         
         activity: {
-            name: 'Wick Studio', // رسالة حالة البوت
-            type: 'WATCHING', // PLAYING, STREAMING, LISTENING, WATCHING, COMPETING
+            name: 'CAS Studio', // رسالة حالة البوت
+            type: 'Streaming', // PLAYING, STREAMING, LISTENING, WATCHING, COMPETING
             status: 'dnd' // online, idle, dnd, invisible
         }
     },
     server: {
-        guildId: 'ID',         // ايدي السيرفر
-        broadcastRoleId: 'ID', // ايدي الرول اللي يستخدم عليها البوت
-        reportChannelId: 'ID' // ايدي الروم اللي يرسل فيه البوت التقارير
+        guildId: '990661091950460928',         // ايدي السيرفر
+        broadcastRoleId: '1255836155178516540', // ايدي الرول اللي يستخدم عليها البوت
+        reportChannelId: '1279293184161091656' // ايدي الروم اللي يرسل فيه البوت التقارير
     },
     broadcast: {
         cooldownTime: 1000, // لا تلعب فيها
@@ -34,4 +34,5 @@ module.exports = {
         error: '#ED4245',      // Red (used for errors)
         neutral: '#5D5D5D'     // Gray (used for neutral messages)
     }
+
 };
